d3a6798f0cbbc4e0da3afd4d309effeb755acff3
Mathew Byrne <mathewbyrne@gmail.com>
2026-02-15 12:29:39 +1100

    initial commit of blog template

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Mathew Byrne",
+	url: "https://mathewbyrne.com/",
+	language: "en",
+	description: "Thoughts on coding, gaming, and art.",
+	author: {
+		name: "Mathew Byrne",
+		email: "mathewbyrne@gmail.com",
+		url: "https://mathewbyrne.com/about-me/"
+	}
+}
